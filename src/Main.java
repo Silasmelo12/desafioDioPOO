0fa56a3f47c03ce4da667a2e3082b25f8a7760f9
Silas <silasmelo12@gmail.com>
2022-11-09 20:51:21 -0300

    Mentoria e Curso.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setTitulo("Descricao curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JavaScript");
+        curso2.setTitulo("Descricao curso javaScript");
+        curso2.setCargaHoraria(8);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descricao da mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1.toString());
+        System.out.println(curso2.toString());
         System.out.println("Hello world!");
     }
 }
